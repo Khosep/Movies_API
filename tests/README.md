181b8adc2bac0d5ec42d19301ab34e3f1558b3e8
Khosep <khose007@yandex.ru>
2024-09-22 23:38:03 +0500

    load tests in local mode

---
+++ tests/README.md
@@ -0,0 +1,11 @@
+## Тесты API
+
+### 1. Локально
+#### 1) Запуск в докере (в папке tests)
+```bash
+docker-compose -f docker-compose_local_tests.yml up -d
+```
+#### 2) Стартуем тесты (в папке tests) в терминале
+```bash
+pytest
+```
--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,3 +1,4 @@
+import os
 from enum import Enum
 from typing import Any
 
@@ -6,9 +7,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class IndexName(str, Enum):
-    GENRES = "genres"
-    MOVIES = "movies"
-    PERSONS = "persons"
+    GENRES = 'genres'
+    MOVIES = 'movies'
+    PERSONS = 'persons'
 
     def __str__(self) -> str:
         return str.__str__(self)
@@ -176,34 +177,47 @@ class IndexSettings(BaseSettings):
         }
     }
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ENV_PATH = os.path.join(BASE_DIR, '.env')
 
 class TestSettings(BaseSettings):
-    model_config = SettingsConfigDict()
 
-    project_name: str = Field(default="test_movies")
+    docker_mode: bool = False
+    if docker_mode:
+        model_config = SettingsConfigDict(
+            env_file=ENV_PATH,
+            env_file_encoding='utf-8',
+            extra='ignore',
+            env_prefix='test_',
+        )
+
+    app_title: str = Field(default='test_movies')
     prefix: str = '/api/v1'
 
-    redis_host: str = Field(default="127.0.0.1")
+    redis_host: str = Field(default='127.0.0.1')
     redis_port: int = Field(default=5379)
 
-    es_host: str = Field(default="127.0.0.1")
+    es_host: str = Field(default='127.0.0.1')
     es_port: int = Field(default=9200)
 
-    project_host: str = Field(default="127.0.0.1")
-    project_port: int = Field(default=8000)
+    project_host: str = Field(default='127.0.0.1')
+    project_port: int = Field(default=8001)
 
     @property
     def app_url(self) -> HttpUrl:
-        return f"http://{self.project_host}:{self.project_port}"
+        return f'http://{self.project_host}:{self.project_port}'
 
     @property
     def es_url(self) -> HttpUrl:
-        return f"http://{self.es_host}:{self.es_port}"
+        return f'http://{self.es_host}:{self.es_port}'
 
     @property
     def redis_dsn(self) -> RedisDsn:
-        return f"redis://{self.redis_host}:{self.redis_port}"
+        return f'redis://{self.redis_host}:{self.redis_port}'
 
 
 test_settings = TestSettings()
 index_settings = IndexSettings()
+
+# for field, value in test_settings:
+#     print(f'{field}: {value}')
